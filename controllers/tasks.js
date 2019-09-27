327111c1491cb230d619184cd948153d3980287d
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-09-27 03:02:47 -0400

    Get task using fields option and reformat

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -8,52 +8,55 @@ module.exports = {
     getAllTasks: (req, res) => {
         mongoose.connect(dbUri, {useNewUrlParser: true})
             .then(() => {
-                return User.findOne({'user.email': req.decoded.user.email}, 'user.name user.email tasks -_id');
+                return User.findOne({'user.email': req.decoded.user.email}, fields);
             }).then(account => {
-                res.send(account);
-            }).catch(err => {
-               console.error("DB error: ", err);
-               res.sendStatus(500);
-            });
+            res.send(account);
+        }).catch(err => {
+            console.error("DB error: ", err);
+            res.sendStatus(500);
+        });
     },
     createTask: (req, res) => {
-        const { title } = req.body;
+        const {title} = req.body;
         console.log(req.body);
         if (title !== undefined && title !== "") {
-             mongoose.connect(dbUri, {useNewUrlParser: true})
-            .then(() => {
-                return User.findOneAndUpdate({'user.email': req.decoded.user.email}, {$push: {tasks: {title} }}, {fields, new: true, lean: true, runValidators: true});
-            }).then((account) => {
+            mongoose.connect(dbUri, {useNewUrlParser: true})
+                .then(() => {
+                    return User.findOneAndUpdate({'user.email': req.decoded.user.email}, {$push: {tasks: {title}}}, {
+                        fields,
+                        new: true,
+                        lean: true,
+                        runValidators: true
+                    });
+                }).then((account) => {
                 res.send(account);
             }).catch(err => {
-               console.error("DB error: ", err);
-               res.sendStatus(500);
+                console.error("DB error: ", err);
+                res.sendStatus(500);
             });
-        }
-        else
-            {
+        } else {
             // Missing required value
             console.error("Missing required parameter", req);
             res.sendStatus(400);
-            }
+        }
     },
     updateTask: (req, res) => {
-        const { title, index } = req.body;
+        const {title, index} = req.body;
         if ((title !== undefined && title !== "") && (index !== undefined && index !== "")) {
             // TODO: Update task
             res.send({user: {"name": "Alice", "email": "Alice@nexus6.com"}, "tasks": ["Task 1", "Task 2"]});
-        }else {
+        } else {
             // Missing required value
             console.error("Missing required parameter", req);
             res.sendStatus(400);
         }
     },
     deleteTask: (req, res) => {
-        const { index } = req.body;
+        const {index} = req.body;
         if (index !== undefined && index !== "") {
             // TODO: Delete task
             res.send({user: {"name": "Alice", "email": "Alice@nexus6.com"}, "tasks": ["Task 1", "Task 2"]});
-        }else {
+        } else {
             // Missing required value
             console.error("Missing required parameter", req);
             res.sendStatus(400);
